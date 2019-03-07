eaf69ef0433973002737c661720404eb0b2fe3d2
lucaxrl <lucas.reginatto.de.lima@gmail.com>
2019-03-07 19:38:49 -0300

    Adding other tests of step by step guide

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,20 +3,21 @@ import ReactDOM from 'react-dom';
 import * as serviceWorker from './serviceWorker';
 serviceWorker.unregister();
 
-const element = <h1>Hello, world</h1>;
+//====================test1=======================
+const element = <h1>Hello, World</h1>;
 ReactDOM.render(element, document.querySelector("#root"));
 
+//===================test2========================
 function tick() {
-    const elementTwo = (
+    var elementTwo = (
         <div        >
             <h1>Hello, World</h1>
             <h2>It is {new Date().toLocaleTimeString()}.</h2>
             <Name name="Lucas"></Name>
             <Age idade="20"></Age>
         </div>
-
     );
-    ReactDOM.render(elementTwo, document.querySelector("#root"));
+    ReactDOM.render(elementTwo, document.querySelector("#root2"));
 }
 setInterval(tick, 1000);
 
@@ -26,11 +27,18 @@ class Name extends React.Component {
     }
 }
 
-class Age extends React.Component{
+class Age extends React.Component {
     render() {
         return <h3>I have, {this.props.idade} years old</h3>
     }
 }
 
+// ===================test3========================
+let myObject = { name: "Lucas", age: 20 };
+function welcomeFunction(props){
+    return <h2>Hello, {props.name}</h2>
+}
+ReactDOM.render(welcomeFunction(myObject), document.querySelector("#root3"));
 
+//===================test4========================
 
